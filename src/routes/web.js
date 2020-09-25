d00368910724ba3775b74e3655db873535508e63
yvansabay <sabayyvan2018@gmail.com>
2020-09-25 22:50:34 +0800

    added webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,12 @@
 import express from "express";
+import chatBotController from "../controllers/chatBotController";
 import homepageController from "../controllers/homepageController"
 let router = express.Router();
 
 let initWebRoutes = (app)=> {
     router.get("/", homepageController.getHomepage);
-
+    router.get("/webhook", chatBotController.getWebhook);
+    router.post("/webhook", chatBotController.postWebhook);
     return app.use("/", router);
 };
 
