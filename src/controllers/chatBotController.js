713454b7b8afe9121f68ebdf233a7593c66dc221
yvansabay <sabayyvan2018@gmail.com>
2020-09-26 00:22:27 +0800

    fixed summary

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -86,9 +86,14 @@ function handleMessage(sender_psid, received_message) {
       // Create the payload for a basic text message
       response = {
         "text": `You sent the message: "${json['Global']['NewConfirmed']}". Now send me an image!`
-        
       }
-      console.log('GG' + covidsummary)
+    }  
+    if (received_message.text) {    
+    
+      // Create the payload for a basic text message
+      response = {
+        "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      }
     }  
     
     // Sends the response message
